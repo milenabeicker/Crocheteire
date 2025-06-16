b45eb97ac60a6ac45e6efc82abd782d8579a7e1a
milenabeicker <milenakessia87@gmail.com>
2025-06-16 17:23:59 -0300

    Initial Commit

---
+++ Crocheteire/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crocheteire
+//
+//  Created by Milena Beicker on 16/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crocheteire/CrocheteireApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrocheteireApp.swift
+//  Crocheteire
+//
+//  Created by Milena Beicker on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CrocheteireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
